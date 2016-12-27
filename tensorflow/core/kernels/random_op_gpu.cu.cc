b6dcfdf5b6970f4c7eb82cf5994000fced4dc59c
Gururaj Rao <gururaja@multicorewareinc.com>
2016-12-27 21:12:17 +0530

    [Fix] use GPUDevice members of hipEigen

--- tensorflow/core/kernels/random_op_gpu.cu.cc
+++ tensorflow/core/kernels/random_op_gpu.cu.cc
@@ -133,9 +133,9 @@ void FillPhiloxRandom<GPUDevice, Distribution>::operator()(
     OpKernelContext*, const GPUDevice& d, random::PhiloxRandom gen,
     typename Distribution::ResultElementType* data, int64 size,
     Distribution dist) {
-  const int32 block_size = d.maxCudaThreadsPerBlock();
+  const int32 block_size = d.maxHipThreadsPerBlock();
   const int32 num_blocks =
-      (d.getNumCudaMultiProcessors() * d.maxCudaThreadsPerMultiProcessor()) /
+      (d.getNumHipMultiProcessors() * d.maxHipThreadsPerMultiProcessor()) /
       block_size;
 
   hipLaunchKernel(HIP_KERNEL_NAME(FillPhiloxRandomKernelLaunch<Distribution>),
